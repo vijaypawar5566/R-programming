4d1ef5ea028025e303387e5cdb741f00770d1b95
Vijay Pawar <114399816+vijaypawar5566@users.noreply.github.com>
2023-06-06 18:57:08 +0530

    About R

---
+++ first.R
@@ -0,0 +1,10 @@
+x=10
+x
+# This is a comment
+print("Hello world !!")
+cat("... This is "," another ", "way!")
+paste("still another way ...", "..great!")
+print(paste("still another way ...", "..great!"))
+
+arr=c(10,10,10,10,1010,10,10,10,10,10,10,10,10,10,10,10,10,10,01,10,10,10)
+arr
